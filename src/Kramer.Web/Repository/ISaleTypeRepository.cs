f7c2b9069a2f972088bd675c842801ee25bfc45e
Marcela Barella silva <marcela.barella@hotmail.com>
2017-02-10 18:19:55 -0200

    Implementa~cao dos repositorios de user request e sale type

---
+++ src/Kramer.Web/Repository/ISaleTypeRepository.cs
@@ -0,0 +1,18 @@
+﻿using Kramer.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Kramer.Repository
+{
+    public class ISaleTypeRepository
+    {
+        IEnumerable<SaleType> All();
+        SaleType GetById(int id);
+        void Add(SaleType saleType);
+        void Delete(int id);
+        void Update(SaleType saleType);
+        void Edit(int id);
+    }
+}
