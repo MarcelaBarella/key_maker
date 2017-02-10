9cd5732b6f21028dbf2b7b19705bb0ec6cba90b7
Ricardo da Verdade Silva <ricardovsilva@msn.com>
2017-02-10 12:48:20 -0200

    Cria a interface do repositorio de UserRequest

---
+++ src/Kramer.Web/Repository/IUserRequestRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Kramer.Domain;
+
+namespace Kramer.Repository
+{
+    public interface IUserRequestRepository
+    {
+        IEnumerable<UserRequest> All();
+        UserRequest Get(int id);
+        void Add(UserRequest userRequest);
+        void Delete(int id);
+        void Update(UserRequest userRequest);
+    }
+}
