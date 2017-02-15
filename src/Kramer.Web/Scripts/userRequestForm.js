e61441bb3aaf2df7e99664d9ce1e1bd9ad159377
Ricardo da Verdade Silva <ricardovsilva@msn.com>
2017-02-15 17:13:43 -0200

    Faz o javascript funcionar

---
+++ src/Kramer.Web/Scripts/userRequestForm.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function () {
+    $('input[name="optradio"]').click(function () {
+        if ($('input[name="optradio"]').is(':checked')) {
+            var radioValue = $("input[name='optradio']:checked").val();
+            if (radioValue == "UsertypeGM") {
+                $("#saletypedrop").prop("disabled", true);
+                $("#saletypedrop").val("null");
+            } else {
+                $("#saletypedrop").prop("disabled", false);
+            }
+        }
+    });
+});
